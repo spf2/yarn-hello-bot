0aa109fb2af905ff6632e7d441ad7ad4a6c3f457
spf2 <steve@farrell.org>
2016-08-24 15:38:19 -0700

    Update test.js
    revise api

--- test.js
+++ test.js
@@ -1,16 +1,15 @@
 function getMessageActions(message) { 
-  return [{
-    title: 'Like', 
-    name: 'like',
-    onSelect: function(bubble, peers) {
-      peers.send();
-    }, 
-    onReceive: function(bubble, value) {
-      if (bubble.badge) {
-        bubble.badge = String(parseInt(bubble.badge) + 1);
-      } else {
-        bubble.badge = "1";
-      }
-    },
-  }]
+  return [
+    ['Like', function(bubble, peers) { peers.send("like"); }],
+  ]
+}
+
+var actionHandlers = {
+  'like': function(bubble, value) {
+    if (bubble.badge) {
+      bubble.badge = String(parseInt(bubble.badge) + 1);
+    } else {
+      bubble.badge = "1";
+    }
+  },
 }
