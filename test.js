e9f6ba9a99267bf818e50c177c235ce07c4283ac
spf2 <steve@farrell.org>
2016-08-25 15:05:36 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,26 +1,18 @@
 function getMessageActions(bubble) {
-  var label = "Like";
-  var key = "like";
-  if (bubble.isToggled("liked")) {
-    label = "Undo Like";
-    key = "unlike";
-  }
-  return [[label, function(bubble, peers) { bubble.toggle("liked"); peers.send(key) }]]
+  return [[
+    "Like", 
+    function(bubble, peers) { peers.send("like", 1) },
+    function(bubble, peers) { peers.send("like", -1) },
+  ]]
 }
 
 var actionHandlers = {
   'like': function(bubble, value) {
+    var incr = parseInt(value);
     if (bubble.badge) {
-      bubble.badge = String(parseInt(bubble.badge) + 1);
+      bubble.badge = String(parseInt(bubble.badge) + incr);
     } else {
-      bubble.badge = "1";
-    }
-  },
-  'unlike': function(bubble, value) {
-    if (!bubble.badge || parseInt(bubble.badge) <= 1) {
-      bubble.badge = "";
-    } else {
-      bubble.badge = String(parseInt(bubble.badge) - 1);
+      bubble.badge = incr;
     }
   },
 }
