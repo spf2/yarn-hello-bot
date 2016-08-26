8d424380b727711169a23f6b79441aae1d404fee
spf2 <steve@farrell.org>
2016-08-26 09:23:33 -0700

    Update test.js
    trying settimeout

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 var yandexUrl = "https://translate.yandex.net/api/v1.5/tr.json/translate";
 var yandexKey = "trnsl.1.1.20160825T224654Z.60d8da3a9f67c52b.ba2dfcc5fcd88c42ed64e266a52c105911fce47c";
 
+function initialize(stream) {
+  console.log(stream.thread.topic);
+  stream.backgroundColorRGB = "#FFFFCC";
+  setTimeout(function() { stream.backgroundRGB = "#FFCCFF" }, 1000);
+}
+
 function getMessageActions(bubble) {
   return [
     [
